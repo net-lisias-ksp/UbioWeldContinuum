fc3a2d135cf1a253e38986355487d8a00c12ec68
Lisias T <me@lisias.net>
2023-06-26 19:44:49 -0300

    Adding `Startup` & `ModuleManagerSupport`

---
+++ Source/UbioWeldingLtd/ModuleManagerSupport.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace UbioWeldingLtd
+{
+	public class ModuleManagerSupport : UnityEngine.MonoBehaviour
+	{
+		public static IEnumerable<string> ModuleManagerAddToModList()
+		{
+			string[] r = {typeof(ModuleManagerSupport).Namespace};
+			return r;
+		}
+	}
+}
---
+++ Source/UbioWeldingLtd/Startup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace UbioWeldingLtd
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    internal class Startup : MonoBehaviour
+    {
+        private void Start()
+        {
+            Log.log("Version {0}", Version.Text);
+
+            try
+            {
+                KSPe.Util.Installation.Check<Startup>();
+            }
+            catch (KSPe.Util.InstallmentException e)
+            {
+                Log.ex(this, e);
+                KSPe.Common.Dialogs.ShowStopperAlertBox.Show(e);
+            }
+        }
+    }
+}
